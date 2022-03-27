0e73b0439053c58cf6587cc4537d994e2389828f
Ponmile Adebisi <ponmileadebisi@gmail.com>
2022-03-27 13:42:17 +0100

    mobile view adjust

--- index.js
+++ index.js
@@ -12,9 +12,21 @@ burger.addEventListener("click", () => {
 
 // Select nav links
 const navLink = document.querySelectorAll(".nav-link");
+console.log("navLink");
 
 navLink.forEach((link) =>
   link.addEventListener("click", () => {
     ul.classList.remove("show");
   })
 );
+
+// scroll to top functionality
+const scrollUp = document.querySelector("#scroll-up");
+
+scrollUp.addEventListener("click", () => {
+  window.scrollTo({
+    top: 0,
+    left: 0,
+    behavior: "smooth",
+  });
+});
